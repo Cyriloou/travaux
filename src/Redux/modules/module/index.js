15d4ee9caf22428e85486eaafdbee37cc9b26998
Cyril <cyril.lacheretz@gmail.com>
2020-03-27 10:09:57 +0100

    redux part I : define items

--- src/Redux/modules/module/index.js
+++
@@ -1,7 +0,0 @@
-import moduleReducer from "./reducer";
-import * as moduleTypes from "./types";
-import * as moduleActions from "./actions";
-
-export { moduleTypes, moduleActions };
-
-export default moduleReducer;
--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import constReducer from 'Redux/modules/const'
+import itemsReducer from 'Redux/modules/items'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
 const rootReducer = combineReducers({
   const: constReducer,
   toastr: toastrReducer, // <- Mounted at toastr.
+  items: itemsReducer,
 })
 
 export default rootReducer
